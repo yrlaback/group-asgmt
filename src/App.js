81035c1fc1bb6f673e89230ef3509327934cb73a
yrlaback <111971911+yrlaback@users.noreply.github.com>
2023-01-10 11:47:51 +0100

    removed home, added header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Home from "./components/Home";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return (
-        <div>
-            <h2>Hello</h2>
-        </div>
-    );
-};
-
-export default Home;
